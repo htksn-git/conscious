5b5ac0616e44e2f77b6486d3db58616d8b224c3f
htksn <vegenai1009@gmail.com>
2023-02-24 16:52:58 +0900

    検索フォーム修正

--- tmp/search-form.php
+++ tmp/search-form.php
@@ -2,5 +2,7 @@
 
 <form method="get" id="searchform" class="search-form" action="<?php bloginfo('url'); ?>">
   <input type="text" name="s" id="s" class="search-form__input" placeholder="">
-  <button type="submit" class="search-form__button">検索</button>
+  <button type="submit" class="search-form__button">
+		<img class="search-form__icon" src="<?php bloginfo('url'); ?>/wp-content/img/common/search.svg" alt="conscious">
+	</button>
 </form>

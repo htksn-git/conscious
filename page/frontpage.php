2d57bd45a2e31f554d08e8dd83c1d4f6d8f6719f
htksn <vegenai1009@gmail.com>
2023-02-27 08:57:18 +0900

    トップページを作成

--- page/frontpage.php
+++ page/frontpage.php
@@ -19,10 +19,79 @@
 </header>
 
 <section class="frontpage-services">
-	<div class="frontpage-services__wrapper">
-		<div class="frontpage-services__card">
-			<h2>更新が容易なウェブサイトづくり</h2>
-			<p class="frontpage-services__text">「WordPress（ワードプレス）を使ってブログを書いて、自社の商品・サービスに関するコンテンツを手軽にアップ」といった内容は</p>
-		</div>
+	<div class="frontpage-services__main-title-en">FEATURED</div>
+	<h2 class="frontpage-services__main-title">特徴・強み</h2>
+
+	<div class="frontpage-services__section--red">
+		<section class="frontpage-services__main">
+			<h3 class="frontpage-services__title">更新が容易なウェブサイトづくり</h3>
+			<p class="frontpage-services__text">当店は、WordPress（ワードプレス）というシステムを利用したウェブサイト制作を主としています。<br>
+「ワードプレスでサイトを用意して、ブログを書いて、自社の商品・サービスに関するコンテンツを発信」といった内容は一般的によく言われます。<br>
+しかし、ブログを効果的かつ継続的にアップしていくことはそれほど簡単なことではありません。<br>
+また商用サイトの場合、ウェブサイトで本当に登録・更新したい箇所は、製品のスペックや価格といった、商品情報など販売・展開していくサービスの具体的な情報であるケースが多いように感じます。</p>
+			<p class="frontpage-services__text">ブログに限定せずこうしたビジネスに直接関係のある箇所も、必要な情報さえ入力すればサイトデザインなど見栄えを損なうことなく、容易に更新できるようカスタマイズすることも可能です。<br>
+言わば、貴社専用の入力テンプレートを予め用意しておき、専門的な知識を持たない方でも必要な情報さえ入力すれば、体裁が整ったページを必要なだけ作成していけるといった環境が実現します。<br>
+また、Excel・スプレッドシートなどで管理しているマスタ情報のデータを加工して、一括でサイトに登録・変更するといった、効率を重視したウェブサイト管理といった方法のご提案も可能です。</p>
+			<p class="frontpage-services__text">これまでに制作した WordPress サイトの数は軽く 100 サイトを超えます。ページボリュームはシンプルな 1 ページのものから、CSV と連携した 1,000 ページを超えるサイトと多岐にわたります。</p>
+			<a href="" class="frontpage-services__btn">ワードプレスについて</a>
+		</section>
+		<aside class="frontpage-services__aside--red">
+			<img src="<?php bloginfo('url'); ?>/wp-content/img/page/frontpage/zzz.png" alt="ワードプレスの管理画面" class="frontpage-services__aside-img">
+			<p class="frontpage-services__aside-text--white">必要な情報さえ入力すれば、簡単にウェブページが出来上がる、オーダーメイドのシステムも構築可能です。</p>
+		</aside>
 	</div>
+
+	<div class="frontpage-services__section--yellow">
+		<section class="frontpage-services__main">
+			<h3 class="frontpage-services__title">自動化プログラムによるバックオフィスの業務効率化</h3>
+			<p class="frontpage-services__text">例えば、月間総計で 3 日かかる作業ルーチンを毎月繰り返し行っていた場合、これをプログラムを使って自動化することで 30 分で終えることができるケースもあります。両者それぞれを年間で換算すると、以下のような差が付くことになります。</p>
+			<dl class=frontpage-services__calc-date>
+				<dt class=frontpage-services__calc-date-title>プログラム導入前</dt>
+				<dd class=frontpage-services__calc-date-detail>
+					<span class="frontpage-services__calc-date-day">3 日</span>
+					<img src="<?php bloginfo('url'); ?>/wp-content/img/icon/calc-cross.svg" alt="" class="frontpage-services__calc-icon-cross">
+					<span class="frontpage-services__calc-date-month">12 ヶ月</span>
+					<img src="<?php bloginfo('url'); ?>/wp-content/img/icon/calc-equal.svg" alt="" class="frontpage-services__calc-icon-equal">
+					<span class="frontpage-services__calc-date-total">36 日</span>
+				</dd>
+				<dt class=frontpage-services__calc-date-title>プログラム導入後</dt>
+				<dd class=frontpage-services__calc-date-detail>
+					<span class="frontpage-services__calc-date-day">30 分</span>
+					<img src="<?php bloginfo('url'); ?>/wp-content/img/icon/calc-cross.svg" alt="" class="frontpage-services__calc-icon-cross">
+					<span class="frontpage-services__calc-date-month">12 ヶ月</span>
+					<img src="<?php bloginfo('url'); ?>/wp-content/img/icon/calc-equal.svg" alt="" class="frontpage-services__calc-icon-equal">
+					<span class="frontpage-services__calc-date-total">360 分</span>
+				</dd>
+			</dl>
+			<p class="frontpage-services__text">前者と後者を比較すると、プログラムを導入する前では年間で約 1.5 ヶ月分もの営業稼働時間を失っていることになります。時間コストは金銭コストに直結するだけでなく、集中力やモチベーションといった部分にも影響を与えます。<br>
+決められたルーチンをトレースする作業であれば、手作業で行うよりもコンピュータに任せた方がスピードは速く、また人的なケアレスミスを防ぐ意味でも多大なメリットがあります。</p>
+			<p class="frontpage-services__text">Excel でも関数やマクロなどを活用すれば複雑なデータの加工はある程度まで可能ですが、Python（パイソン）のようなプログラミング言語を駆使することで、より一歩踏み込んだ業務改善を実現できます。<br>
+これは単純にデータを加工する意味合いだけでなく、現状の問題点を洗い出した上で、データを集計・分類・加工・出力することなども含まれます。</p>
+			<p class="frontpage-services__text">これらの改善事例のひとつに、商品出荷の際にボトルネックとなっていた部分に対して専用のプログラムを導入することで、デリバリーにかかる時間が従来の 2/3 程度まで圧縮できたケースもあります。<br>
+また、この際にタスク自体を細分化できたことにより、スタッフの教育コストが大きく下がった結果、人員確保といった採用面における負担が以前より減少したといった二次的な恩恵もありました。<br>
+現状のバックオフィスが抱える問題点をより良くするためのプログラム開発について、詳細は以下のページをご覧ください。</p>
+			<a href="" class="frontpage-services__btn">プログラムによる業務効率化</a>
+		</section>
+		<aside class="frontpage-services__aside--yellow">
+			<img src="<?php bloginfo('url'); ?>/wp-content/img/page/frontpage/program.svg" alt="プログラミングによるデータ連携のイメージ画像" class="frontpage-services__aside-img">
+			<p class="frontpage-services__aside-text--black">Excel の情報をウェブサイトに適合するように変換したり、CSV データの内容から納品書や越境 EC におけるインボイスなど各種帳票の作成を一括で行ったり、手作業では時間がかかり過ぎる業務を効率化します。</p>
+		</aside>
+	</div>
+
+	<div class="frontpage-services__section--green">
+		<section class="frontpage-services__main">
+			<h3 class="frontpage-services__title">豊富なプラットフォームに対応した EC サイト制作</h3>
+			<p class="frontpage-services__text">これまで、Shopify・楽天市場・Yahoo! ショッピング・カラーミーショップ・BASE・STORES など、各種 e コマースのプラットフォームを利用したオンラインショップの構築経験がありますが、中でも Shopify・楽天市場（楽天 GOLD）の構築事例が多いです。</p>
+			<p class="frontpage-services__text">Shopify は、テンプレートタグおよび Liquid では対応しきれない部分を JavaScript で補い、既存の機能からさらに発展させた形でのショップ運用も可能です。<br>
+また、ルートドメインを Shopify で運用しつつ、サブディレクトリに WordPress をインストールして、ショッピングサイト／自社サイトをシームレスに連結するといった構築事例もあります。</p>
+			<p class="frontpage-services__text">楽天市場は実質的に「楽天 GOLD」のカスタマイズを指しますが、年間で約 30 回ほど開催される各種セールのランディングページに、JavaScript によるタイマー機能を設けてページの表示を制御するといった、楽天のスケジュールに準拠した対応も可能です。<br>
+また、商品タイトルや販売価格といった商品情報は CSV によって書き換えが可能ですが、リモートサーバーにインストールした Linux を活用することで、24 時間・365 日好きなタイミングで商品情報を自動で変更するといった体制を設けることも可能です。</p>
+			<a href="" class="frontpage-services__btn">EC サイト制作について</a>
+		</section>
+		<aside class="frontpage-services__aside--green">
+			<img src="<?php bloginfo('url'); ?>/wp-content/img/page/frontpage/e-commerce.svg" alt="eコマースのイメージ画像" class="frontpage-services__aside-img">
+			<p class="frontpage-services__aside-text--white">Shopify・楽天市場・Yahoo! ショッピング・カラーミーショップ・BASE・STORES など、各種 e コマースのプラットフォームを利用したオンラインショップの構築経験があります。</p>
+		</aside>
+	</div>
+
 </section>
